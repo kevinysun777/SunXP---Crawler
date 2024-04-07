6a17e85a5e51bfeaace523b9044fccdd9c531e4a
kevinysun777 <156815619+kevinysun777@users.noreply.github.com>
2024-04-06 21:37:00 -1000

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.adobe.com/products/premiere.html",
-  match: "https://www.adobe.com/**",
-  maxPagesToCrawl: 10000,
-  outputFileName: "output.json",
-  maxTokens: 1000,
+  url: "https://phaseshawaii.com/",
+  match: "https://phaseshawaii.com/**",
+  maxPagesToCrawl: 500,
+  outputFileName: "phaseshawaii_crawl.json",
+  maxTokens: 200000,
 };
